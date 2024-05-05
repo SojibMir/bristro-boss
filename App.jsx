f20813dd8943976d922c61f3c5d3d419aafb72e5
Shofikul Islam Sojib <83161951+SojibMir@users.noreply.github.com>
2024-05-05 17:29:39 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <button className="btn btn-outline mt-2 my-2">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
